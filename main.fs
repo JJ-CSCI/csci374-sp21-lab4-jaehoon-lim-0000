3a4cdd2674b5d795e8bec628c9b1c1d49cf3adbe
jaehoon lim <wogns7378@gmail.com>
2021-03-10 22:06:17 +0000

    finished

--- main.fs
+++ main.fs
@@ -11,7 +11,10 @@ let rec prod (lst:int list) =
 
 // Problem 2
 let rec map f (lst:int list) =
-   []
+   if List.isEmpty lst
+  then []
+  else (f (List.head lst))::(map f (List.tail lst)) 
+
     // write your solution here
     
 
@@ -20,16 +23,24 @@ let rec map f (lst:int list) =
 // Problem 3
 let rec odd (lst:int list) =
     // write your solution here
-   if List.head lst % 2 = 1
-   then lst
-   else odd(List.tail lst)
+   if List.isEmpty lst
+   then []
+   else 
+   let head = List.head lst
+   let tail = List.tail lst 
+   if head % 2 = 1
+   then head::(odd tail) 
+   else odd tail
    
     
 // Problem 4
 let rec filter f lst =
     // write your solution here
-    
-    if (f > 3)
-    then f :: List.head lst
-              
-    else filter(List.tail lst)
+   if List.isEmpty lst 
+   then []
+   else
+   let head = List.head lst
+   let tail = List.tail lst
+   if f head
+   then head::(filter f tail)
+   else filter f tail
